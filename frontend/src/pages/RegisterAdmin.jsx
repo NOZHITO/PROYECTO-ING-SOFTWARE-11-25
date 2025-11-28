fd65c44e269041f6cab797342df45f68ba0284e7
AshCraft23 <90273368+AshCraft23@users.noreply.github.com>
2025-11-28 13:00:05 -0500

    Update RegisterAdmin.jsx

--- frontend/src/pages/RegisterAdmin.jsx
+++ frontend/src/pages/RegisterAdmin.jsx
@@ -23,33 +23,26 @@ const RegisterAdmin = () => {
     });
   };
 
+
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setSuccess("");
-    setLoading(true);
+  e.preventDefault();
 
-    try {
-      const response = await api.post("/auth/register_admin", {
-        name: formData.name,
-        email: formData.email,
-        password: formData.password,
-        confirmPassword: formData.confirmPassword,
-      });
+  try {
+    const response = await api.post("/auth/register_admin", {
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+      confirmPassword: formData.confirmPassword,
+    });
 
-      if (response.status === 201) {
-        setSuccess("Administrador creado correctamente ✅");
-        setTimeout(() => navigate("/login"), 2000);
-      }
+    setSuccess("Administrador creado correctamente.");
+    setTimeout(() => navigate("/login"), 2000);
 
-    }  catch (err) {
-        console.log("ERROR COMPLETO:", err);
-        console.log("ERROR RESPONSE:", err.response);
-        console.log("ERROR DATA:", err.response?.data);
-
-        setError(JSON.stringify(err.response?.data) || "Error desconocido");
-    }
-
+  } catch (err) {
+    console.log("ERROR BACKEND:", err.response?.data);
+    setError(err.response?.data?.msg || "Error al registrar administrador.");
+  }
+};
 
     } finally {
       setLoading(false);
