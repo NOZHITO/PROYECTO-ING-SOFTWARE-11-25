2f23db8497d9820d6a8f4a88db8484659fb18f09
AshCraft23 <90273368+AshCraft23@users.noreply.github.com>
2025-11-28 01:24:35 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,21 +2,14 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
-    if SQLALCHEMY_DATABASE_URI and "sslmode" not in SQLALCHEMY_DATABASE_URI:
-        if "?" in SQLALCHEMY_DATABASE_URI:
-            SQLALCHEMY_DATABASE_URI += "&sslmode=require"
-        else:
-            SQLALCHEMY_DATABASE_URI += "?sslmode=require"
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = os.getenv("SECRET_KEY", "clave_secreta_backup")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_backup")
+    SECRET_KEY = "clave_secreta_para_el_proyecto_granja_camarones"
+    JWT_SECRET_KEY = "jwt_clave_super_secreta"
 
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
